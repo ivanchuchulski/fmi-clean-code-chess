f5c1cb16efddc1b656a84ffb78052f8d3765fb39
Ivan Chuchulski <zake6@abv.bg>
2020-04-11 19:21:55 +0300

    improve foolsMate test method, add scholarsMate test

--- test/TwoPlayerChessGameTest.java
+++ test/TwoPlayerChessGameTest.java
@@ -1,5 +1,6 @@
 import Chess.Board.BoardCoordinate;
 import Chess.Board.ChessGame;
+import Chess.Pieces.PieceEnums.PieceColor;
 import Console.*;
 import org.junit.After;
 import org.junit.Before;
@@ -30,7 +31,7 @@ public class TwoPlayerChessGameTest {
     }
 
     @Test
-    public void testFoolsMateEndsGame() {
+    public void testFoolsMate() {
         String[] foolsMateMoves = new String[]{"F2-F3", "E7-E6", "G2-G4", "D8-H4"};
 
         for (String move : foolsMateMoves) {
@@ -43,9 +44,28 @@ public class TwoPlayerChessGameTest {
             }
         }
 
-//        boardDisplay.printBoard(game.getBoard());
+        boardDisplay.printBoard(game.getBoard());
 
-        assert (game.isFinished());
+        assert (game.isFinished() && game.getLastPlayedMovePieceColor().equals(PieceColor.Black));
+    }
+
+    @Test
+    public void testScholarsMate() {
+        String[] scholarsMateMoves = new String[]{"E2-E4", "E7-E5", "F1-C4", "B8-C6", "D1-H5", "G8-F6", "H5-F7"};
+
+        for (String move : scholarsMateMoves) {
+            BoardCoordinate from = inputHandler.getSource(move);
+            BoardCoordinate to = inputHandler.getDestination(move);
+
+            boolean movePlayed = game.playMove(from, to);
+            if (!movePlayed) {
+                fail("Should be legal move");
+            }
+        }
+
+        boardDisplay.printBoard(game.getBoard());
+
+        assert (game.isFinished() && game.getLastPlayedMovePieceColor().equals(PieceColor.White));
     }
 
     @Test
